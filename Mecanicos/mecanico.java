b29a88c78cd94d6c22faeec773b293328b61477a
Ricardo Gallardo <gallardoespinozar@gmail.com>
2024-08-08 15:07:05 -0600

    Reparaciones
    se crean las atributos y las funciones basicas y se crea un Enum para estado

---
+++ Mecanicos/mecanico.java
@@ -0,0 +1,49 @@
+
+
+package Mecanicos;
+
+/**
+ * ID, nombre, especialidad
+ *
+ * @author bizco
+ */
+public class mecanico {
+
+    private String Id;
+    private String nombre;
+    private String especialidad;
+
+    public String getId() {
+        return Id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public mecanico(String Id, String nombre, String especialidad) {
+        this.Id = Id;
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+    }
+
+    public mecanico() {
+        this.Id = "";
+        this.nombre = "";
+        this.especialidad = "";
+    }
+
+    @Override
+    public String toString() {
+        return "mecanico{" + "Id=" + Id + ", nombre=" + nombre + ", especialidad=" + especialidad + '}';
+    }
+
+}
---
+++ Vehiculos/vehiculo.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vehiculos;
+
+/**
+ *
+ * @author jcone
+ */
+public class vehiculo {
+    private String matricula;
+    private String modelo;
+    private String marca;
+    private int añoFabricacion;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public int getAñoFabricacion() {
+        return añoFabricacion;
+    }
+
+    public vehiculo(String matricula, String modelo, String marca, int añoFabricacion) {
+        this.matricula = matricula;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.añoFabricacion = añoFabricacion;
+    }
+
+    public vehiculo() {
+        this("", "", "", 0);
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" + "matricula=" + matricula + ", modelo=" + modelo + ", marca=" + marca + ", a\u00f1oFabricacion=" + añoFabricacion + '}';
+    }
+} 
+
