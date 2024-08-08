51a55453f7803aff8959d256d2e19e5e6871662c
JavierOD <jjod@outlook.es>
2024-08-08 14:01:10 -0600

    Creacion de clase Clientes
    Se crea la clase clientes con sus respectivos metodos

---
+++ Clientes/Cliente.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clientes;
+
+/**
+ *
+ * @author javierortizdobles
+ */
+public class Cliente {
+    private int id;
+    private String nombre;
+    private String telefono;
+    private String direccion;
+
+    public Cliente(int id, String nombre, String telefono, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public Cliente() {
+        this(0,"","","");
+    }
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "id=" + id + ", nombre=" + nombre + ", telefono=" + telefono + ", direccion=" + direccion + '}';
+    }
+    
+    
+    
+            
+    
+}
